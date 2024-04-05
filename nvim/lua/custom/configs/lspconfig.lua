a6fee49801bd4b0fa07a6f9965b02e23cb9f3740
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-04-04 19:05:07 -0600

    Adding custom options to the tsserver

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "tsserver", "emmet_ls", "pylsp" }
+local servers = { "emmet_ls", "pylsp" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -10,5 +10,17 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    hostInfo = "neovim",
+    preferences = {
+      -- Removes the external modules from completetion
+      includeCompletionsForModuleExports = false,
+    },
+  },
+}
 
 lspconfig.rust_analyzer.setup {}
