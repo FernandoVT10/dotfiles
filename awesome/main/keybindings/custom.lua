061a5e7cdd9b13eabf3d591093153ed36e06ae28
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2024-11-11 16:22:12 -0600

    Remove random wallpaper when pressing Mod+w

--- awesome/main/keybindings/custom.lua
+++ awesome/main/keybindings/custom.lua
@@ -1,20 +1,6 @@
 local awful = require("awful")
 local gears = require("gears")
 
-local get_files_from_folder = require("main.getFilesFromFolder")
-
--- Defined on the rc.lua
-local wallpaper_list = get_files_from_folder(WALLPAPER_FOLDER)
-
-local function changeWallpaper ()
-  math.randomseed(os.time());
-  local active_wallpaper = math.random(0, #wallpaper_list)
-
-  for s in screen do
-    gears.wallpaper.maximized(wallpaper_list[active_wallpaper], s)
-  end
-end
-
 local customBindings = gears.table.join(
   -- Standard program
   awful.key({ MOD_KEY }, "Return", function () awful.spawn(TERMINAL .. " tmux") end, {
@@ -29,11 +15,7 @@ local customBindings = gears.table.join(
 
   -- Volume
   awful.key({ }, "XF86AudioRaiseVolume", function () awful.util.spawn("amixer set Master 5%+") end),
-  awful.key({ }, "XF86AudioLowerVolume", function () awful.util.spawn("amixer set Master 5%-") end),
-  awful.key({ MOD_KEY }, "w", changeWallpaper, {
-    description = "Change to the next wallpaper",
-    group = "screen"
-  })
+  awful.key({ }, "XF86AudioLowerVolume", function () awful.util.spawn("amixer set Master 5%-") end)
 )
 
 return customBindings
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -4,7 +4,6 @@ local gears = require("gears")
 local awful = require("awful")
 local beautiful = require("beautiful")
 local menubar = require("menubar")
-local get_files_from_folder = require("main.getFilesFromFolder")
 
 require("awful.autofocus")
 
@@ -25,24 +24,13 @@ awful.layout.layouts = {
 
 menubar.utils.terminal = TERMINAL
 
-WALLPAPER_FOLDER = string.format("%s/Pictures/Wallpaper", os.getenv("HOME"))
-
-local wallpaper_list = get_files_from_folder(WALLPAPER_FOLDER)
-local wallpaper = wallpaper_list[math.random(0, #wallpaper_list)]
-
-local function set_wallpaper(s)
-  gears.wallpaper.maximized(wallpaper, s, false)
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 local set_bar = require("main.bar")
 
 awful.screen.connect_for_each_screen(function(s)
-  set_wallpaper(s)
   set_bar(s)
 end)
+
+awful.spawn({"feh --bg-scale ~/Pictures/Wallpaper/Yuukari.png"})
 
 require("main.keybindings")
 require("main.rules")
