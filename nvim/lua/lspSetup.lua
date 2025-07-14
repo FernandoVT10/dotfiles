5337909f9e533498675858d8f78d08bfce3199e3
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2025-07-14 14:18:05 -0600

    Added "Marksman" lsp server

--- nvim/lua/lspSetup.lua
+++ nvim/lua/lspSetup.lua
@@ -1,7 +1,6 @@
 -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
 
-lspconfig["ts_ls"].setup {
-  -- capabilities = capabilities
-}
+lspconfig["ts_ls"].setup {}
 lspconfig["gopls"].setup {}
+lspconfig["marksman"].setup {}
