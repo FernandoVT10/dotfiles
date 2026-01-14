5e658277507ee12b5554b9fb8341cb3dbb9467c3
FernandoVT10 <vacatamayo321@gmail.com>
2026-01-13 21:02:03 -0600

    Adapt change on neovim API

--- nvim/lua/lspSetup.lua
+++ nvim/lua/lspSetup.lua
@@ -1,6 +1,1 @@
--- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local lspconfig = require("lspconfig")
-
-lspconfig["ts_ls"].setup {}
-lspconfig["gopls"].setup {}
-lspconfig["marksman"].setup {}
+vim.lsp.enable("marksman")
